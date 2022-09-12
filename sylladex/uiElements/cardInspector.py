61d77db4c8168f0a3edf2138c01b678e02709ceb
Luna <linkzersmash@gmail.com>
2022-09-12 09:54:48 -0600

    Added all label text

--- sylladex/uiElements/cardInspector.py
+++ sylladex/uiElements/cardInspector.py
@@ -9,4 +9,4 @@ class CardInspector(UIBase):
 
         self.font = pg.font.Font("sylladex/uiElements/asset/MISC/DisposableDroidBB.ttf", 18)
 
-        self.create_appearance(colorKey = True, image = [f'sylladex/uiElements/asset/{UIBase.get_modus()}/CARD_INSPECTOR.png', [0, 0]], texts = [["NAME", [36, 48], "center"], ["TRAIT 1", [50, 84], "center"], ["TRAIT 2", [50, 110], "center"], ["ITEMKIND", [52, 134], "center"], ['GRIST TYPE', [64, 158], 'center']])
+        self.create_appearance(colorKey = True, image = [f'sylladex/uiElements/asset/{UIBase.get_modus()}/CARD_INSPECTOR.png', [0, 0]], texts = [["NAME", [36, 48], "center"], ["TRAIT 1", [51, 84], "center"], ["TRAIT 2", [51, 109], "center"], ["ITEMKIND", [52, 134], "center"], ['GRIST TYPE', [64, 159], 'center'], ['EFFECTIVE', [68, 184], 'center'], ['INEFFECTIVE', [68, 209], 'center'], ['INSPECT INFORMATION', [159, 305], 'center'], ['CST', [30, 330], 'center'], ['DMG', [30, 355], 'center'], ['CODE', [79, 391], 'center'], ['TIER', [223, 391], 'center'], ['1', [247, 121], 'center'], ['2', [247, 146], 'center'], ['3', [247, 171], 'center'], ['BD', [247, 196], 'center']])
